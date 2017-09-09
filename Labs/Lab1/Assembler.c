9e2fa1a17f5df5e9f11c77e3e60075f27fc259de
crs94 <rscamila1994@gmail.com>
2017-09-08 23:07:27 -0300

    Testing structs

--- Labs/Lab1/Assembler.c
+++ Labs/Lab1/Assembler.c
@@ -2,17 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-    char str1;
+struct instTab {
+    char name[7];
+    char opCode[3];
+    int opNum;
+    struct instTable *pointer;
+};
+
+struct instTab *createInsts();
+void openFile(char *filename);
+
+int main() {
+
     char filename[50] = "TestFiles/bin.asm";
+    struct instTab *headInst;
+
+    openFile(filename);
+
+    headInst = createInsts();
+    printf("\n%s\t%s\t%d", headInst->name, headInst->opCode, headInst->opNum);
+    getchar();
+
+    return 0;
+}
+
+struct instTab *createInsts() {
+
+    struct instTab *head = NULL;
+    head = (struct instTab*) malloc(sizeof(struct instTab));
+
+    strcpy(head->name, "test");
+    strcpy(head->opCode, "16");
+    head->opNum = 1;
+
+    return head;
+}
+
+
+void openFile(char *filename) {
+    char str1;
     FILE *fp;
 
     if ((fp = fopen(filename, "r")) == NULL) {
         printf("404 Not Found!");
         exit(1);
     }
-
     if (fp == NULL) {
         printf("File couldn't be opened.");
     }
@@ -21,8 +55,5 @@ int main()
         printf("%c", str1);
     }
     printf("\nEOF");
-
     fclose(fp);
-
-    return 0;
 }
