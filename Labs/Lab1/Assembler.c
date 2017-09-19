46c6c2522d4c8fe8038c13f3844aa1ea86c4a895
crs94 <rscamila1994@gmail.com>
2017-09-19 17:07:34 -0300

    Added some of the structs for the tables

--- Labs/Lab1/Assembler.c
+++ Labs/Lab1/Assembler.c
@@ -3,44 +3,77 @@
 #include <string.h>
 
 struct instTab {
-    char name[7];
-    char opCode[3];
-    int opNum;
-    struct instTable *pointer;
+    char inst[7];
+    char opcode[3];
+    int operator;
 };
 
-struct instTab *createInsts();
+struct dirTab {
+	char dir[7];
+	int operator;
+	int size;
+};
+
+struct mnt {
+	char name[101];
+	struct mdt *beginning;
+	struct mnt *next;
+};
+
+struct mdt {
+	char line[51];
+	struct mdt *next;
+};
+
+struct symTab {
+	char label[101];
+	int memWhere;
+	struct places *list;
+	int defined;
+	int vector;
+	struct symTab *next;
+};
+
+struct equTab {
+	char label[101];
+	int value;
+	struct equTab *next;
+};
+
+struct instTab **createInsts();
 void openFile(char *filename);
 
 int main() {
 
     char filename[50] = "TestFiles/bin.asm";
-    struct instTab *headInst;
+    struct instTab **headInst;
 
     openFile(filename);
 
     headInst = createInsts();
-    printf("\n%s\t%s\t%d", headInst->name, headInst->opCode, headInst->opNum);
+    printf("\n%s\t%s\t%d", headInst[0]->inst, headInst[0]->opcode, headInst[0]->operator);
     getchar();
 
     return 0;
 }
 
-struct instTab *createInsts() {
+struct instTab **createInsts() {
 
-    struct instTab *head = NULL;
-    head = (struct instTab*) malloc(sizeof(struct instTab));
+    struct instTab *ptr[2];
+    ptr[0] = NULL;
+    ptr[0] = (struct instTab*) malloc(sizeof(struct instTab));
 
-    strcpy(head->name, "test");
-    strcpy(head->opCode, "16");
-    head->opNum = 1;
+    strcpy(ptr[0]->inst, "test");
+    strcpy(ptr[0]->opcode, "16");
+    ptr[0]->operator = 1;
 
-    return head;
+    return ptr;
 }
 
 
 void openFile(char *filename) {
-    char str1;
+    char str1[50];
+    int i = 0;
     FILE *fp;
 
     if ((fp = fopen(filename, "r")) == NULL) {
@@ -49,11 +82,14 @@ void openFile(char *filename) {
     }
     if (fp == NULL) {
         printf("File couldn't be opened.");
+        exit(1);
     }
 
-    while (fscanf(fp, "%c", &str1) == 1){
-        printf("%c", str1);
+    /*while (fscanf(fp, "%c", &str1) == 1){*/
+    while (fgets(str1, 50, fp) != NULL){
+        printf("%s", str1);
+        i++;
     }
-    printf("\nEOF");
+    printf("\nEOF and i = %d", i);
     fclose(fp);
 }
