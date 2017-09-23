12bcf84f56ba9d3a56957f573617323c7d7b5183
crs94 <rscamila1994@gmail.com>
2017-09-23 19:03:11 -0300

    getToken implemented (still need some tests)

--- Labs/Lab1/Preprocess.c
+++ Labs/Lab1/Preprocess.c
@@ -10,13 +10,14 @@ struct equTab {
 };
 
 int getLine(FILE *fp, char *lineBuffer);
-void getToken(char *lineBuffer, char *tokenBuffer);
+int getToken(char *lineBuffer, char *tokenBuffer, int p);
 
 int main() {
 
 	char line[501], token[101];
 	char filename[] = "TestFiles/SB_test_getline.asm";
 	FILE *fp;
+	int linePos = 0;
 
 	if ((fp = fopen(filename, "r")) == NULL) {
         printf("404 Not Found!");
@@ -24,7 +25,9 @@ int main() {
     }
 
     while (getLine(fp, line)) {
-    	;
+    	while (linePos = getToken(line, token, linePos)) {
+			;
+    	}
     }
 
     if (fclose(fp) == 0) {
@@ -40,7 +43,7 @@ int getLine(FILE *fp, char *lineBuffer) {
 	char c;
 	int n = 0;
 
-	while (((c = fgetc(fp)) != '\n') && (n <= 500) && (c != EOF)) {
+	while (((c = fgetc(fp)) != '\n') && (n <= 499) && (c != EOF)) {
 		if (n == 0) {
 			// Ignores initial blanks
 			while ((c == ' ') || (c == '\t')) {
@@ -49,7 +52,7 @@ int getLine(FILE *fp, char *lineBuffer) {
 		}
 		if ((lineBuffer[n - 1] == ' ') && (n != 0)) {
 			// Ignores useless blanks in line
-			while ((c == ' ') || (c == '\t') ) {
+			while ((c == ' ') || (c == '\t')) {
 				c = fgetc(fp);
 			}
 		}
@@ -57,24 +60,43 @@ int getLine(FILE *fp, char *lineBuffer) {
 			c = toupper(c);
 		}
 		lineBuffer[n] = c;
-		printf("%c", lineBuffer[n]);
 		n++;
 	}
-
-	if (n <= 500) {
-		lineBuffer[++n] = '\0';		
+	if (n <= 499) {
+		lineBuffer[n] = '\n';		
+		lineBuffer[++n] = '\0';
 	}
+	//printf("%s\n", lineBuffer);
 
-	if (c == EOF) {
-		return 0;
-	}
-	else {
+	if (c != EOF) {
 		return 1;
 	}
+	else {
+		return 0;
+	}
 }
 
-void getToken(char *lineBuffer, char *tokenBuffer) {
+int getToken(char *lineBuffer, char *tokenBuffer, int p) {
 
+	int n = 0;
+	
+	while ((!isspace(lineBuffer[p])) && (n < 101)) {
+		tokenBuffer[n] = lineBuffer[p];
+		n++;
+		p++;
+	}
+	tokenBuffer[n] = '\0';
+	printf("%s ", tokenBuffer);
 
-
+	if (lineBuffer[p] == '\n') {
+		return 0;
+	}
+	p++;
+	return p;
 }
+
+/*void clearArray(char *buffer) {
+
+	for (int n = 0; n <= )
+
+}*/
